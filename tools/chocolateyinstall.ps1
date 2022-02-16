e4b476a6b01fad60532192d416b4f54e383a00c7
mychocopackagesATfreemailDOThu <mychocopackages@freemail.hu>
2022-02-16 04:19:24 +0100

    1.0

---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,34 @@
+﻿$ErrorActionPreference = 'Stop'; 
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'http://dl.uconomix.com/ExifPurge.zip'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'exe'
+  url           = $url
+  softwareName  = 'exif-purge*' 
+  checksum      = '4DA7C418D9C08D5A41141001B29F8EBED8CC8A6EE2ED2BAF22CE84BC4D9D65E9'
+  checksumType  = 'sha256'
+}
+
+# Create folder in Program Files for the application and unzip to this
+$programfiles = [Environment]::GetFolderPath("ProgramFiles")
+$unzippath = Join-Path $programfiles $env:ChocolateyPackageTitle
+Install-ChocolateyZipPackage $packageName $url $unzippath
+
+$executable = "ExifPurge.exe"
+
+# Add Desktop shortcut
+$desktoppath = [Environment]::GetFolderPath("Desktop")
+$shortcutfilename = "$env:ChocolateyPackageTitle.lnk"
+$shortcutpath = Join-Path  $desktoppath $shortcutfilename
+$executablepath = Join-Path $unzippath $executable
+Install-ChocolateyShortcut -shortcutFilePath $shortcutpath -targetPath $executablepath
+
+# Add StartMenu shortcut
+$startmenupath = [Environment]::GetFolderPath('StartMenu')
+$shortcutfilename = "$env:ChocolateyPackageTitle.lnk"
+$shortcutpath = Join-Path  "$startmenupath\Programs" $shortcutfilename
+$executablepath = Join-Path $unzippath $executable
+Install-ChocolateyShortcut -shortcutFilePath $shortcutpath -targetPath $executablepath
---
+++ tools/chocolateyuninstall.ps1
@@ -0,0 +1,43 @@
+﻿$ErrorActionPreference = 'Stop';
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  softwareName  = 'exif-purge*' 
+}
+
+[array]$key = Get-UninstallRegistryKey -SoftwareName $packageArgs['softwareName']
+
+if ($key.Count -eq 1) {
+  $key | % {
+    $packageArgs['file'] = "$($_.UninstallString)" 
+
+    if ($packageArgs['fileType'] -eq 'MSI') {
+      $packageArgs['silentArgs'] = "$($_.PSChildName) $($packageArgs['silentArgs'])"
+      $packageArgs['file'] = ''
+    }
+    Uninstall-ChocolateyPackage @packageArgs
+  }
+} elseif ($key.Count -eq 0) {
+  Write-Warning "$packageName has already been uninstalled by other means."
+} elseif ($key.Count -gt 1) {
+  Write-Warning "$($key.Count) matches found!"
+  Write-Warning "To prevent accidental data loss, no programs will be uninstalled."
+  Write-Warning "Please alert package maintainer the following keys were matched:"
+  $key | % {Write-Warning "- $($_.DisplayName)"}
+}
+
+# Remove the program's folder from Program Files
+$programfiles = [Environment]::GetFolderPath("ProgramFiles")
+$unzippath = Join-Path $programfiles $env:ChocolateyPackageTitle
+Remove-Item $unzippath -Recurse
+
+# Remove Start Menu shortcut
+$startmenupath = [Environment]::GetFolderPath('StartMenu')
+$shortcutfilename = "$env:ChocolateyPackageTitle.lnk"
+$shortcutpath = Join-Path  "$startmenupath\Programs" $shortcutfilename
+Remove-Item $shortcutpath -Recurse
+
+# Remove Desktop shortcut
+$desktoppath = [Environment]::GetFolderPath("Desktop")
+$shortcutfilename = "$env:ChocolateyPackageTitle.lnk"
+$shortcutpath = Join-Path  $desktoppath $shortcutfilename
+Remove-Item $shortcutpath -Recurse
